10f06238867123dd976dd92bb636a68c84e8e647
enes taha sarı <ets.34.es@gmail.com>
2023-02-27 14:41:08 +0300

    axios interceptors

--- client/src/utils/appAxios.js
+++ client/src/utils/appAxios.js
@@ -3,17 +3,17 @@ import { BASE_URL } from "../constants";
 const appAxios = axios.create({
   baseURL: BASE_URL,
   withCredentials: false,
-  // headers: {
-  //   "Content-Type": "application/json",
-  // },
+  headers: {
+    "Content-Type": "application/json",
+  },
   //   Authorization: `Bearer: ${access_token}`,
 });
-// appAxios.interceptors.request.use((config) => {
-//   config.headers = {
-//     ...config.headers,
-//     // Authorization: `Bearer: ${localStorage.access_token}`,
-//   };
-//   return config;
-// });
+appAxios.interceptors.request.use((config) => {
+  config.headers = {
+    ...config.headers,
+     Authorization: `Bearer: ${localStorage.access_token}`,
+  };
+  return config;
+});
 
 export default appAxios;
