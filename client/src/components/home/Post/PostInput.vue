d9b5a8fd57512b0278e76c89be04d67fcb75a98c
enes taha sarı <ets.34.es@gmail.com>
2023-03-15 20:24:58 +0300

    postInput profile image bug fixed

--- client/src/components/home/Post/PostInput.vue
+++ client/src/components/home/Post/PostInput.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { ref } from 'vue';
+import { BASE_URL } from '../../../constants';
+
 import SendPostModal from "./SendPostModal.vue"
 const props = defineProps({
     currentUser: {
@@ -13,7 +15,7 @@ const openSendPostModal = () => {
 </script>
 <template>
     <div class="flex justfiy-between items-center space-x-2">
-        <img :src="`http://localhost:3000/uploads/${currentUser?.profile_image}`" alt="" class="rounded-full w-14 " />
+        <img :src="`${BASE_URL}/uploads/${currentUser?.profile_image}`" alt="" class="rounded-full w-14 " />
         <input @focus="openSendPostModal" type="text" placeholder="Gönderi başlat"
             class="border flex-1 border-muted placeholder:font-semibold placeholder:text-muted hover:cursor-pointer hover:bg-gray-200 transition-all duration-300 py-3 px-4 rounded-full" />
     </div>
