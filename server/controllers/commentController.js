ba0d91234a00068975476a1192a2f860fd802e6e
enes taha sarı <ets.34.es@gmail.com>
2023-04-05 22:40:07 +0300

    comment notif. and no result image

--- server/controllers/commentController.js
+++ server/controllers/commentController.js
@@ -1,18 +1,29 @@
 const asyncErrorWrapper = require("express-async-handler");
 const CustomError = require("../helpers/error/CustomError");
 const Comment = require("../models/Comment");
-const Post = require("../models/Post");
+const { sendNotification } = require("../helpers/libraries/sendNotification");
 
 const addNewComment = asyncErrorWrapper(async (req, res, next) => {
   const userData = req.body;
-  const comment = await (
-    await Comment.create({
-      ...userData,
-      post: req.params.post_id,
-      user: req.user.id,
-    })
-  ).populate("user");
+  const createdComment = await Comment.create({
+    ...userData,
+    post: req.params.post_id,
+    user: req.user.id,
+  });
+  const comment = await Comment.findById(createdComment._id)
+    .populate("user")
+    .populate({
+      path: "post",
+      select: "user",
+    });
 
+  if (req.user.id !== comment.post.user) {
+    await sendNotification({
+      message: `gönderinize yorum yaptı.`,
+      sender: req.user.id,
+      receiver: comment.post.user,
+    });
+  }
   return res.status(200).json({
     success: true,
     comment,
